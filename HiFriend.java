160bbeb42ba25b9cc06e61404b26f9a296d4a4ff
Josh Taylor <34116753+TaylorJosh@users.noreply.github.com>
2022-05-02 13:37:16 -0400

    Printing Practice Problems commit
    Printing Practice Problems commit

---
+++ HiFriend.java
@@ -0,0 +1,9 @@
+public class HiFriend {
+    public static void main(String[] args) throws Exception {
+        String name = "Josh Taylor";
+        String greeting = "Hi, ";
+
+
+        System.out.println(greeting + name + "!");
+    }
+}
---
+++ MyNameIs.java
@@ -0,0 +1,7 @@
+public class MyNameIs {
+    public static void main(String[] args) throws Exception {
+        System.out.println("My Name Is");
+        System.out.println("Joshua");
+        System.out.println("Taylor");
+    }
+}
